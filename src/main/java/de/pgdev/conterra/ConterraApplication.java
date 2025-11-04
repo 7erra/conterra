411bbea75ee2dd2c4f1534605b5e6eb6051ec9c6
7erra <ichbinterra@gmail.com>
2025-11-04 13:40:32 +0100

    Init

---
+++ src/main/java/de/pgdev/conterra/ConterraApplication.java
@@ -0,0 +1,13 @@
+package de.pgdev.conterra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConterraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConterraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/pgdev/conterra/ConterraApplicationTests.java
@@ -0,0 +1,13 @@
+package de.pgdev.conterra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConterraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
