96630a78f9978809bec0ef700eefabbc762aca56
7erra <ichbinterra@gmail.com>
2025-11-04 14:06:41 +0100

    Return random values for forecast

--- src/main/java/de/pgdev/conterra/forecast/ForecastController.java
+++ src/main/java/de/pgdev/conterra/forecast/ForecastController.java
@@ -5,12 +5,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
-import java.time.LocalDate;
+import java.util.Random;
 
 @RestController
 public class ForecastController {
+    Random rand = new Random();
+
+    private double round(double number) {
+        return (double)Math.round(number * 100) / 100;
+    }
+
     @GetMapping("/forecast")
     public Forecast forecast(@RequestParam String lat, @RequestParam String lon) {
-        return new Forecast(Instant.now().getEpochSecond(), 13.76, 20, 69);
+        double temp = round(rand.nextDouble(60) - 20);
+        double feels_like = round(rand.nextDouble(60) - 20);
+        int humidity = rand.nextInt(100);
+        return new Forecast(Instant.now().getEpochSecond(), temp, feels_like, humidity);
     }
 }
